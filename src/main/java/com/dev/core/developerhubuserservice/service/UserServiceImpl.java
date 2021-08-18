1790ca0d868caf2dc7255b5d40ded4cb39e3bbf5
Steven8519 <engineers2526@gmail.com>
2021-08-18 03:47:39 -0400

    added password encoder bcrypt. closes #5

--- src/main/java/com/dev/core/developerhubuserservice/service/UserServiceImpl.java
+++ src/main/java/com/dev/core/developerhubuserservice/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.dev.core.developerhubuserservice.service;
 import com.dev.core.developerhubuserservice.domain.User;
 import com.dev.core.developerhubuserservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,14 +11,18 @@ public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
 
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         super();
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
--- src/test/java/com/dev/core/developerhubuserservice/UserControlerTest.java
+++ src/test/java/com/dev/core/developerhubuserservice/UserControlerTest.java
@@ -15,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -39,7 +41,7 @@ public class UserControlerTest {
     public void postUser_whenUserIsValid_receiveOK() {
         User user = createValidUser();
         ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 
     @Test
@@ -50,10 +52,13 @@ public class UserControlerTest {
     }
 
     @Test
-    public void postUser_whenUserIsValid_receiveSuccessMessage() {
+    public void postUser_whenUserIsValid_passwordIsHashedInDatabase() {
         User user = createValidUser();
-        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+        List<User> users = userRepository.findAll();
+        User inDB = users.get(0);
+        assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
+
     }
 
     private User createValidUser() {
